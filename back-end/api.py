6627450b1e2553a412ec4320a256b9b0bb210ad9
Iago Monfredini <iagomonfredini46171@gmail.com>
2025-10-13 16:17:53 -0300

    rota listar filmes

--- back-end/api.py
+++ back-end/api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from funcao import inserir_filmes, listar_filmes, atualizar_filmes, deletar_filmes
 
 #Rodar o fastapi
-# python -m uvicorn api.py: app -reload
+# python -m uvicorn api: app --reload
 
 #Testar api Fastapi
 # /docs > Documentação Swagger
@@ -24,3 +24,23 @@ def home():
 def criar_filmes(titulo: str, genero: str, ano: int, avaliacao: float):
     inserir_filmes(titulo, genero, ano, avaliacao)
     return {"mensagem": "Filme adicionado com sucesso!"}
+
+@app.get("/filmes")
+def exibir_filmes():
+    filmes = listar_filmes()
+    lista = []
+    for linha in filmes:
+        lista.append({
+                "id": linha[0], 
+                "titulo": linha[1],
+                "genero": linha[2],
+                "ano": linha[3],
+                "avaliacao": linha[4]
+                })
+    return {"filmes": lista}
+
+
+
+
+
+
